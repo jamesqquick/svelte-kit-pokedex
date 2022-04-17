1dc36859f77a832649d4e6f1e2fb61894c9935bf
James Q Quick <james.q.quick@gmail.com>
2022-04-17 16:53:36 -0500

    Removing outdated svelte target config

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,6 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
 };
 
 export default config;
