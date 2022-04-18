5811754e1161c0949697c3927b9cda430847c452
Halftime Harry <ddinsmore8@gmail.com>
2022-04-18 14:07:37 +0000

    Sucseful at getting the data

--- src/stores/nflstore.js
+++ src/stores/nflstore.js
@@ -2,18 +2,12 @@ import { writable } from 'svelte/store';
 
 export const nfl = writable([]);
 
+console.log(nfl)
 const fetchNfl = async () => {
-	const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+	const url = `https://api.sportsdata.io/v3/nfl/scores/json/Schedules/2019?key=82091bd8383e46caafe746f559bc5c0c`;
 	const res = await fetch(url);
 	const data = await res.json();
-	const loadedNfl = data.results.map((data, index) => {
-		return {
-			name: data.name,
-			id: index + 1,
-			image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1
-				}.png`
-		};
-	});
-	nfl.set(loadedNfl);
+	return data
 };
 fetchNfl();
+
