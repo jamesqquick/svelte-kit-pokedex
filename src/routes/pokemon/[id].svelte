b86167ffda8537debaa464541d64ba97b164bfc2
Igor <ivanwaesberghe@outlook.com>
2022-04-29 02:53:51 -0700

    Update [id].svelte
    Changed pokeman type to use the updated pokeman types.

--- src/routes/pokemon/[id].svelte
+++ src/routes/pokemon/[id].svelte
@@ -8,7 +8,8 @@
 </script>
 <script>
     export let pokeman;
-    const type = pokeman.types[0].type.name;
+    //const type = pokeman.types[0].type.name;
+    const types = pokeman.types.map(type => type.type.name);
 </script>
 <svelte:head>
 	<title>Pokedex - {pokeman.name}</title>
@@ -17,7 +18,7 @@
 <div class="flex flex-col items-center">
 
     <h1 class="text-4xl text-center my-8 uppercase">{pokeman.name}</h1>
-    <p>Type: <strong>{type}</strong> | Height: <strong>{pokeman.height}</strong>
+    <p>Types: <strong>{types}</strong> | Height: <strong>{pokeman.height}</strong>
         | Weight: <strong>{pokeman.weight}</strong>
     </p>
     <img class="card-image" src={pokeman.sprites['front_default']} 
